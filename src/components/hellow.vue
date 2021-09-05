88b75a5287312c415e421d0f3ede5c0f77dfb1ff
gu-th <gutonghao@foxmail.com>
2021-09-05 16:37:33 +0800

    feat: 调整

---
+++ src/components/hellow.vue
@@ -0,0 +1,43 @@
+<!--
+ * @Author       : guth
+ * @Date         : 2021-09-05 16:34:20
+ * @LastEditors  : guth
+ * @LastEditTime : 2021-09-05 16:34:21
+ * @FilePath     : \vite-vue3-ts-template\src\components\hellow.vue
+ * @Description  : 
+-->
+<template>
+  <div class="fd-info">
+    <p>欢迎使用 .</p>
+    <span class="fd-title">This is a template project for </span>
+    <span class="fd-text"> Vite + Vue3.* + TS .</span>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss" scoped>
+.fd-info {
+  position: relative;
+  top: 120px;
+  text-align: center;
+}
+p {
+  font-size: 32px;
+  margin-bottom: 40px;
+}
+.fd-title {
+  font-size: 28px;
+  text-align: center;
+}
+.fd-text {
+  font-size: 32px;
+  font-style: italic;
+  font-weight: 600;
+  text-align: center;
+  text-shadow: 5px 5px 8px #a66fee;
+  color: #a66fee;
+}
+</style>
--- src/views/home.vue
+++ src/views/home.vue
@@ -2,44 +2,18 @@
  * @Author       : guth
  * @Date         : 2021-08-30 18:00:30
  * @LastEditors  : guth
- * @LastEditTime : 2021-09-02 12:14:18
+ * @LastEditTime : 2021-09-05 16:35:59
  * @FilePath     : \vite-vue3-ts-template\src\views\home.vue
  * @Description  :
 -->
 <template>
-  <div class="fd-info">
-    <p>欢迎使用 .</p>
-    <span class="fd-title">This is a template project for </span>
-    <span class="fd-text"> Vite + Vue3.* + TS .</span>
-  </div>
-  <!-- <common-nav></common-nav> -->
+  <hellow></hellow>
 </template>
 
 <script lang="ts" setup>
-// import commonNav from "../components/common-nav.vue";
+import hellow from "../components/hellow.vue";
 
 </script>
 
 <style lang="scss" scoped>
-.fd-info {
-  position: relative;
-  top: 120px;
-  text-align: center;
-}
-p {
-  font-size: 32px;
-  margin-bottom: 40px;
-}
-.fd-title {
-  font-size: 28px;
-  text-align: center;
-}
-.fd-text {
-  font-size: 32px;
-  font-style: italic;
-  font-weight: 600;
-  text-align: center;
-  text-shadow: 5px 5px 8px #a66fee;
-  color: #a66fee;
-}
 </style>
